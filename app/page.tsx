2a3b0af9f159eafb8d168b48603aedd7416f2c4f
Jan Šimeček <simecekjann@gmail.com>
2025-08-03 13:20:25 +0200

    Comment out default components for now

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,15 @@ import { BrainIcon } from 'lucide-react';
 
 import { CopyRegistryButton } from '@/components/copy-registry-button';
 import { OpenInV0Button } from '@/components/open-in-v0-button';
-import PokemonPage from '@/registry/default/blocks/complex-component/page';
-import { ExampleForm } from '@/registry/default/blocks/example-form/example-form';
-import { ExampleCard } from '@/registry/default/blocks/example-with-css/example-card';
-import { HelloWorld } from '@/registry/default/blocks/hello-world/hello-world';
 import { IconButton } from '@/registry/default/ui/icon-button';
-// This page displays items from the custom registry.
-// You are free to implement this with your own design as needed.
 
 export default function Home() {
   return (
     <div className='max-w-3xl mx-auto flex flex-col min-h-svh px-4 py-8 gap-8'>
       <header className='flex flex-col gap-1'>
-        <h1 className='text-3xl font-bold tracking-tight'>Custom Registry</h1>
+        <h1 className='text-3xl font-bold tracking-tight'>Utima UI</h1>
         <p className='text-muted-foreground'>
-          A custom registry for distributing code using shadcn.
+          A custom registry for Utima UI components using shadcn.
         </p>
       </header>
       <main className='flex flex-col flex-1 gap-8'>
@@ -27,7 +21,7 @@ export default function Home() {
             </h2>
             <div className='flex gap-2'>
               <CopyRegistryButton name='icon-button' />
-              <OpenInV0Button name='hello-world' className='w-fit' />
+              <OpenInV0Button name='icon-button' className='w-fit' />
             </div>
           </div>
           <div className='flex items-center justify-center min-h-[400px] relative gap-3'>
@@ -43,7 +37,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative'>
+        {/* <div className='flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative'>
           <div className='flex items-center justify-between'>
             <h2 className='text-sm text-muted-foreground sm:pl-3'>
               A simple hello world component
@@ -92,7 +86,7 @@ export default function Home() {
           <div className='flex items-center justify-center min-h-[400px] relative'>
             <ExampleCard />
           </div>
-        </div>
+        </div> */}
       </main>
     </div>
   );
