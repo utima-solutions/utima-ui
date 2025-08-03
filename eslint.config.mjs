9fe29bd498acf697d34b4f6b14c3e7a46331736e
Jan Šimeček <simecekjann@gmail.com>
2025-08-03 12:41:29 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import baseConfig from '@utima/eslint-config';
+import { next } from '@utima/eslint-config/configs';
+
+export default [
+  ...baseConfig,
+  ...next,
+  {
+    rules: {
+      'react/no-multi-comp': 'off',
+    },
+  },
+];
